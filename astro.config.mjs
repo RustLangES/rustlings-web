3635d9b41d11591cd5db976d0f24d2dd8c397483
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-10-08 03:11:32 -0400

    feat: add tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  integrations: [tailwind()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true
