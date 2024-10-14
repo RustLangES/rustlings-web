b7d64fe898194eccee86ddc14a7edabb1d052373
Wilovy09 <srwilovy09@gmail.com>
2024-10-13 20:02:55 -0600

    feat: Create a function for a requests

---
+++ src/services/request.ts
@@ -0,0 +1,22 @@
+/**
+ * Hacer una petición a una API
+ * @param {string} method - Método de la petición en mayúsculas Ej: GET, POST, PUT, DELETE
+ * @param {string} url - URL completa de la API
+ * @param {Object} payload - Body/Cuerpo de la petición
+ * @returns {Promise} - Promesa con la respuesta de la petición
+ */
+export async function request(
+  method: string,
+  url: string,
+  payload: Object,
+): Promise<any> {
+  const response = await fetch(url, {
+    method,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: method !== "GET" ? JSON.stringify(payload) : null,
+  });
+  return response.json();
+}
