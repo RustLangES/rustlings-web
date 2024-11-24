0136a1de9d6c7e18eb71362f7d4c83182798fbef
Wilovy09 <srwilovy09@gmail.com>
2024-11-24 13:47:51 -0600

    :art: Format & :sparkles: Add CodeMirror component

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
-    <nav>
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Test</li>
-        </ul>
-    </nav>
+  <nav>
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Test</li>
+    </ul>
+  </nav>
 </template>
