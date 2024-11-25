452b48b524f76ab5cbe1c56bdc59cf09f59fbd3c
JonathanACO <jonathanandresco@gmail.com>
2024-11-24 19:14:23 -0600

    se da formato y se cambia m-[10px] por m-2.5

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -3,29 +3,40 @@ import { CircleChevronRight, CircleChevronLeft } from "lucide-vue-next";
 </script>
 
 <template>
-  <main class="grid grid-cols-1 lg:grid-cols-2 m-[10px]">
+  <main class="grid grid-cols-1 lg:grid-cols-2 gap-2.5 m-2.5">
     <section
-      class="bg-light-bg h-[calc(100vh-20px)] lg:mr-[5px] border border-stroke-color rounded-[10px] flex flex-col">
+      class="bg-light-bg h-[calc(100vh-20px)] border border-stroke-color rounded-[10px] flex flex-col"
+    >
       <ContentDoc v-slot="{ doc }" class="flex-grow">
-        <div class="scroll-container flex-grow m-[10px] overflow-auto">
+        <div class="scroll-container flex-grow m-2.5 overflow-auto">
           <ContentRenderer :value="doc" />
         </div>
-        <div buttons class="flex justify-between m-[10px] mt-auto">
-          <a :href="doc && doc.previousPath ? `/${doc.previousPath}` : null" :class="{
-            'pointer-events-none text-gray-400': !doc || !doc.previousPath,
-          }" class="flex items-center">
+        <div buttons class="flex justify-between m-2.5 mt-auto">
+          <a
+            :href="doc && doc.previousPath ? `/${doc.previousPath}` : undefined"
+            :class="{
+              'pointer-events-none text-gray-400': !doc || !doc.previousPath,
+            }"
+            class="flex items-center"
+          >
             <CircleChevronLeft :size="30" />
           </a>
 
-          <a :href="doc && doc.nextPath ? `/${doc.nextPath}` : null" :class="{
-            'pointer-events-none text-gray-400': !doc || !doc.nextPath,
-          }" class="flex items-center">
+          <a
+            :href="doc && doc.nextPath ? `/${doc.nextPath}` : undefined"
+            :class="{
+              'pointer-events-none text-gray-400': !doc || !doc.nextPath,
+            }"
+            class="flex items-center"
+          >
             <CircleChevronRight :size="30" />
           </a>
         </div>
       </ContentDoc>
     </section>
-    <section class="bg-light-bg h-[calc(100vh-20px)] xs:mt-[10px] lg:mt-0 lg:ml-[5px] p-[10px] border border-stroke-color rounded-[10px]">
+    <section
+      class="bg-light-bg h-[calc(100vh-20px)] p-2.5 border border-stroke-color rounded-[10px]"
+    >
       <CodeMirror />
     </section>
   </main>
@@ -58,4 +69,3 @@ import { CircleChevronRight, CircleChevronLeft } from "lucide-vue-next";
   border-radius: 10px;
 }
 </style>
-
