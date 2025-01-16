ca747228cc1aac336ac1c3671d8c3f883b649961
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-01-16 19:51:24 -0400

    fix: build in development

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,15 @@ export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: false },
   nitro: { preset: "cloudflare-pages" },
+
   modules: [
     "@nuxtjs/color-mode",
     "@nuxt/content",
     "@nuxtjs/tailwindcss",
-    "nitro-cloudflare-dev",
-    'nuxt-clarity-analytics',
+    //"nitro-cloudflare-dev",
+    "nuxt-clarity-analytics",
   ],
+
   content: {
     highlight: {
       theme: {
@@ -18,4 +20,6 @@ export default defineNuxtConfig({
       langs: ['rust', 'shell', 'viml', 'shellsession'] // instala el lang faltante
     },
   },
+
+  compatibilityDate: "2025-01-16",
 });
