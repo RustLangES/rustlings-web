3c145b84f512d49606fdddf4f768a0791a68c6d7
Bryan Lundberg <bryanlundberg@outlook.com>
2025-10-25 16:27:03 -0600

    feat: Add Playground component with Rust CodeMirror editor and themingfeat: Add Rust autocompletion support and create Playground component

---
+++ src/helpers/theme.ts
@@ -0,0 +1,126 @@
+// Original repo: https://github.com/CMOISDEAD/theme-gruvbox-dark
+
+import { HighlightStyle, syntaxHighlighting } from "@codemirror/language"
+import type { Extension } from "@codemirror/state"
+import { EditorView } from "@codemirror/view"
+import { tags as t } from "@lezer/highlight"
+
+const chalky = "#d79921",
+	coral = "#fb4934",
+	cyan = "#83a598",
+	invalid = "#ffffff",
+	ivory = "#abb2bf",
+	stone = "#7d8799",
+	malibu = "#458588",
+	sage = "#8ec07c",
+	whiskey = "#fe9019",
+	violet = "#b16286",
+	darkBackground = "#1d2021",
+	highlightBackground = "#1d2021",
+	tooltipBackground = "#3c3836",
+	selection = "#504945",
+	cursor = "#fabd2f"
+
+export const gruvboxDarkTheme = EditorView.theme(
+	{
+		"&": {
+			color: ivory,
+			fontSize: "16px",
+		},
+
+		".cm-content": {
+			caretColor: cursor,
+		},
+
+		".cm-cursor, .cm-dropCursor": { borderLeftColor: cursor },
+		"&.cm-focused .cm-selectionBackground, .cm-selectionBackground, .cm-content ::selection": {
+			backgroundColor: selection,
+		},
+
+		".cm-panels": { backgroundColor: darkBackground, color: ivory },
+		".cm-panels.cm-panels-top": { borderBottom: "2px solid black" },
+		".cm-panels.cm-panels-bottom": { borderTop: "2px solid black" },
+
+		".cm-searchMatch": {
+			backgroundColor: "#72a1ff59",
+			outline: "1px solid #457dff",
+		},
+		".cm-searchMatch.cm-searchMatch-selected": {
+			backgroundColor: "#6199ff2f",
+		},
+
+		".cm-activeLine": { backgroundColor: highlightBackground },
+		".cm-selectionMatch": { backgroundColor: "#aafe661a" },
+
+		"&.cm-focused .cm-matchingBracket, &.cm-focused .cm-nonmatchingBracket": {
+			backgroundColor: "#bad0f847",
+			outline: "1px solid #515a6b",
+		},
+
+		".cm-gutters": {
+			backgroundColor: "transparent",
+			color: stone,
+			border: "none",
+		},
+
+		".cm-activeLineGutter": {
+			backgroundColor: highlightBackground,
+		},
+
+		".cm-foldPlaceholder": {
+			backgroundColor: "transparent",
+			border: "none",
+			color: "#ddd",
+		},
+
+		".cm-tooltip": {
+			border: "none",
+			backgroundColor: tooltipBackground,
+		},
+		".cm-tooltip .cm-tooltip-arrow:before": {
+			borderTopColor: "transparent",
+			borderBottomColor: "transparent",
+		},
+		".cm-tooltip .cm-tooltip-arrow:after": {
+			borderTopColor: tooltipBackground,
+			borderBottomColor: tooltipBackground,
+		},
+		".cm-tooltip-autocomplete": {
+			"& > ul > li[aria-selected]": {
+				backgroundColor: highlightBackground,
+				color: ivory,
+			},
+		},
+	},
+	{ dark: true },
+)
+
+export const gruvboxDarkHighlightStyle = HighlightStyle.define([
+	{ tag: t.keyword, color: violet },
+	{
+		tag: [t.name, t.deleted, t.character, t.propertyName, t.macroName],
+		color: coral,
+	},
+	{ tag: [t.function(t.variableName), t.labelName], color: malibu },
+	{ tag: [t.color, t.constant(t.name), t.standard(t.name)], color: whiskey },
+	{ tag: [t.definition(t.name), t.separator], color: ivory },
+	{
+		tag: [t.typeName, t.className, t.number, t.changed, t.annotation, t.modifier, t.self, t.namespace],
+		color: chalky,
+	},
+	{
+		tag: [t.operator, t.operatorKeyword, t.url, t.escape, t.regexp, t.link, t.special(t.string)],
+		color: cyan,
+	},
+	{ tag: [t.meta, t.comment], color: stone },
+	{ tag: t.strong, fontWeight: "bold" },
+	{ tag: t.emphasis, fontStyle: "italic" },
+	{ tag: t.strikethrough, textDecoration: "line-through" },
+	{ tag: t.link, color: stone, textDecoration: "underline" },
+	{ tag: t.heading, fontWeight: "bold", color: coral },
+	{ tag: [t.atom, t.bool, t.special(t.variableName)], color: whiskey },
+	{ tag: [t.processingInstruction, t.string, t.inserted], color: sage },
+	{ tag: t.invalid, color: invalid },
+])
+
+export const gruvbox: Extension = [gruvboxDarkTheme, syntaxHighlighting(gruvboxDarkHighlightStyle)]
