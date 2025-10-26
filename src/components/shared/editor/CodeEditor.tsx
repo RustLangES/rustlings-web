bf45b5ca874831b85e9129539285f4b41d9454ed
Kevin Villegas Pérez <villegasdeveloper@outlook.es>
2025-10-25 20:36:29 -0500

    refactor(editor): rename playground to code editor and clean up unused constants
    -Move the playground component to a more appropriately named code editor component
    Remove unused rust support constants and operators from keywords file.
    -Update imports in content page to use new component name.

--- src/components/shared/editor/CodeEditor.tsx
+++ src/components/shared/editor/CodeEditor.tsx
@@ -1,22 +1,23 @@
 import CodeMirror from "@uiw/react-codemirror"
-import React from "react"
+import { useCallback, useState } from "react"
 import { rust } from "../../../helpers/config"
 import { gruvbox } from "../../../helpers/theme"
 
-function Playground() {
-	const [value, setValue] = React.useState('fn main() {\n    println!("Hola, mundo!");\n}\n')
-	const onChange = React.useCallback((val, viewUpdate) => {
+export function CodeEditor() {
+	const [value, setValue] = useState('fn main() {\n    println!("Hola, mundo!");\n}\n')
+
+	const onChange = useCallback((val: string) => {
 		setValue(val)
 	}, [])
+
 	return (
 		<CodeMirror
 			value={value}
 			extensions={[rust()]}
 			theme={gruvbox}
 			onChange={onChange}
-			className={"flex-1 min-h-[200px] overflow-y-auto scroll-container"}
+			className="flex-1 min-h-[200px] overflow-y-auto scroll-container"
 			id="editor"
 		/>
 	)
 }
-export default Playground
