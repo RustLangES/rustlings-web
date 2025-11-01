88876619337cb5e5b3a96efbe518bd8ddda30fe3
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-01 17:06:49 -0600

    refactor: update ReactPanelContainer styles and improve resize handle structure

--- src/features/content/react/ReactPanelContainer.tsx
+++ src/features/content/react/ReactPanelContainer.tsx
@@ -17,15 +17,15 @@ export default function ReactPanelContainer({
 	defaultLayout = [50, 50],
 }: PanelContainerProps) {
 	return (
-		<PanelGroup direction={direction} className="h-full min-h-0">
+		<PanelGroup direction={direction} className="h-full min-h-0 ">
 			<Panel defaultSize={defaultLayout[0]} className="min-h-0">
 				{first}
 			</Panel>
-			<div className={"w-2 flex justify-center items-center rounded-full cursor-col-resize mx-auto"}>
-				<PanelResizeHandle>
+			<PanelResizeHandle className={"flex justify-center items-center"}>
+				<div className={"bg-primary mx-auto  cursor-col-resize grow flex items-center justify-center"}>
 					<IconGripVertical size={14} className={direction === "vertical" ? "rotate-90" : ""} />
-				</PanelResizeHandle>
-			</div>
+				</div>
+			</PanelResizeHandle>
 			<Panel defaultSize={defaultLayout[1]} className="min-h-0">
 				{second}
 			</Panel>
