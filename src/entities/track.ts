2177854c0116a4e7aa16a0306df704c6984a724b
wilovy <emiliano@adquiere.co>
2026-03-03 22:36:41 -0600

    fix: format

--- src/entities/track.ts
+++ src/entities/track.ts
@@ -1,10 +1,10 @@
 export type Track = {
-  id: string;
-  title: string;
-  description: string;
-  href: string;
-  lessons: number;
-  badge: string;
-  disabled: boolean;
-  slugs: string[];
-};
+	id: string
+	title: string
+	description: string
+	href: string
+	lessons: number
+	badge: string
+	disabled: boolean
+	slugs: string[]
+}
