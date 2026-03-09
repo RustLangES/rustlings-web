c4ff69499d186e8709a6f2e045bfdeb9d0585d2e
Emiliano Maldonado Garza <68057133+Wilovy09@users.noreply.github.com>
2026-03-09 12:27:17 -0600

    fix(images): Change Cloudflare adapter image service to passthrough

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import Icons from "unplugin-icons/vite"
 // https://astro.build/config
 export default defineConfig({
 	output: "server",
-	adapter: cloudflare({ imageService: "cloudflare" }),
+	adapter: cloudflare({ imageService: "passthrough" }),
 	i18n: {
 		defaultLocale: "es",
 		locales: ["es", "en"],
